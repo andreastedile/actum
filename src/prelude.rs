3d8aa7b9c72965d291db24cf8dd9cf6019ebc30e
andreastedile <andrea.stedile@studenti.unitn.it>
2024-05-30 11:36:20 +0200

    Add ActorCell in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,6 @@
 pub use crate::actor::Actor;
 pub use crate::actor_bounds::ActorBounds;
 pub use crate::actor_cell::actor_task::RunTask;
+pub use crate::actor_cell::ActorCell;
 pub use crate::actor_ref::ActorRef;
 pub use crate::actum;
