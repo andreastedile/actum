7a5333b6e1e939d7db22257597e9c81f25d2f505
andreastedile <andrea.stedile@studenti.unitn.it>
2025-03-27 17:29:25 +0100

    Document RecvFutureStateMachine

--- src/actor_cell/test_actor.rs
+++ src/actor_cell/test_actor.rs
@@ -15,11 +15,17 @@ use std::future::{poll_fn, Future};
 use std::task::{ready, Poll};
 
 pub struct TestExtension<M> {
-    recv_effect_sender: mpsc::Sender<RecvEffectFromActorToTestkit<M>>,
+    /// used to send recv effects from the actor under test to the corresponding testkit.
+    pub recv_effect_sender: mpsc::Sender<RecvEffectFromActorToTestkit<M>>,
+    /// used to receive recv effects from the testkit to the actor.
     recv_effect_receiver: mpsc::Receiver<RecvEffectFromTestkitToActor<M>>,
-    spawn_effect_sender: mpsc::Sender<SpawnEffectFromActorToTestkit>,
-    spawn_effect_receiver: mpsc::Receiver<SpawnEffectFromTestkitToActor>,
+
     state: RecvFutureStateMachine,
+
+    /// used to send spawn effects from the actor under test to the corresponding testkit.
+    spawn_effect_sender: mpsc::Sender<SpawnEffectFromActorToTestkit>,
+    /// used to receive spawn effects from the testkit to the actor.
+    spawn_effect_receiver: mpsc::Receiver<SpawnEffectFromTestkitToActor>,
 }
 
 impl<M> TestExtension<M> {
@@ -39,10 +45,48 @@ impl<M> TestExtension<M> {
     }
 }
 
+/// The [Actor::recv] future can be dropped before being polled to completion; for example, when it
+/// races with a short timeout.
+/// This fact, and the fact that the result of the future is temporarily transferred to the testkit,
+/// makes future not cancel safe.
+///
+/// This state machine makes the future cancel safe.
 #[derive(Debug, Eq, PartialEq)]
 pub enum RecvFutureStateMachine {
+    /// We have called [Actor::recv] and obtained a future.
+    /// We can now poll or drop it.
+    ///
+    /// - If we poll the future, it internally polls the [MessageReceiver::recv] future, which is cancel safe,
+    /// to obtain the [Recv] value.
+    /// If the latter resolves into a value, we send it to the testkit and go to S1;
+    /// otherwise, we remain in S0 and arrange our future to be polled again.
+    ///
+    /// - If we drop the future, we remain in S0.
+    /// If we create a new future afterward, we will resume from there.
     S0,
+    /// The testkit has received the [Recv] value from us.
+    /// We can now poll or drop our future.
+    ///
+    /// - If we poll the future, it attempts to receive a response with the value back from the testkit.
+    /// If we receive those, we resolve our future into the value and go to S0;
+    /// otherwise, we remain in S1 and arrange our future to be polled again.
+    ///
+    /// - If we drop the future, we go to S2.
+    /// If we create a new future afterward, we will resume from there.
     S1,
+    /// The testkit still has the [Recv] value we sent to it.
+    /// We can now poll or drop our future.
+    ///
+    /// - If we poll the future, it attempts to receive a response and the value back from the testkit.
+    /// If we receive those, we resend the value to the testkit and go to S1;
+    /// otherwise, we remain in S2 and arrange our future to be polled again.
+    /// The reason we resend the value is that the response we have just received corresponds to a
+    /// future which we previously dropped and is thus now meaningless.
+    /// Note that **we resend the very same value**, because the [Recv] could contain a message which
+    /// we do not want to lose.
+    ///
+    /// - If we drop the future, we remain in S2.
+    /// If we create a new future afterward, we will resume from there.
     S2,
 }
 
