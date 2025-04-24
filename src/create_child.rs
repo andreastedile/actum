19d9af6208e765f80555463858e4cc2bb90f2ee5
andreastedile <andrea.stedile@studenti.unitn.it>
2025-04-24 19:00:34 +0200

    Update create_child doc

--- src/create_child.rs
+++ src/create_child.rs
@@ -32,10 +32,77 @@ pub trait CreateChild: Sized + Send + 'static {
         Ret: Send + 'static;
 
     /// Creates a child actor.
-    /// The actor should then be spawned onto the runtime of choice.
+    /// The documentation of [actum](crate::actum) applies to this function as well.
     ///
-    /// See the [actum](crate::actum) function for passing a function pointer, passing a closure and
-    /// for passing arguments to the actor.
+    /// From within the child actor, you can [create](CreateChild::create_child) new child actors of its own.
+    /// Therefore, this function effectively instantiates an actor tree hierarchy rooted in the child actor.
+    ///
+    /// Like the actum function, it returns a struct containing the [ActorRef] of the root actor (generic over parameter `M`) and a special control structure.
+    /// The control structure has a method that returns a future which runs the child actor, joins the child actors of its own (if any), and resolves with the value returned by the child actor (generic over parameter `Ret`).
+    /// Once this future resolves, the entire actor tree hierarchy rooted in the child actor has returned.
+    ///
+    /// You can await the future to obtain the value returned by the child actor.
+    /// Otherwise, if your actor needs to do other work while the child actor is running, you can spawn the future in the background using your runtime of choice.
+    /// In the case of Tokio, this can be done with the [spawn](https://docs.rs/tokio/latest/tokio/task/fn.spawn.html) function:
+    /// you can await the [JoinHandle](https://docs.rs/tokio/latest/tokio/task/struct.JoinHandle.html) to obtain the value returned by the child actor.
+    /// If you do not do so and your actor returns, Actum will still join the child actor and the child actor of its own (if any).
+    /// Therefore, your actor will never be outlived by a child actor in any case.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use actum::prelude::*;
+    ///
+    /// #[derive(Debug)]
+    /// pub struct Request {
+    ///     n: u32,
+    ///     reply_to: ActorRef<Response>,
+    /// }
+    ///
+    /// #[derive(Debug)]
+    /// pub struct Response {
+    ///     n: u32,
+    /// }
+    ///
+    /// async fn root_actor<C, R>(mut cell: C, mut receiver: R, me: ActorRef<Response>) -> (C, ())
+    /// where
+    ///     C: CreateChild,
+    ///     R: ReceiveMessage<Response>,
+    /// {
+    ///     let ActorToSpawn { task, mut actor_ref } = cell.create_child(child_actor).await;
+    ///     let _handle = tokio::spawn(task.run_task());
+    ///
+    ///     // do other work...
+    ///
+    ///     let request = Request { n: 1, reply_to: me };
+    ///     actor_ref.try_send(request).unwrap();
+    ///
+    ///     let response = receiver.recv().await.into_message().unwrap();
+    ///     println!("received response: {:?}", response.n);
+    ///
+    ///     // handle.await.unwrap(); // not necessary
+    ///     (cell, ())
+    /// }
+    ///
+    /// async fn child_actor<C, R>(cell: C, mut receiver: R, _me: ActorRef<Request>) -> (C, ())
+    /// where
+    ///     C: CreateChild,
+    ///     R: ReceiveMessage<Request>,
+    /// {
+    ///     let mut request = receiver.recv().await.into_message().unwrap();
+    ///     println!("received request: {:?}", request.n);
+    ///     let response = Response { n: request.n * 2 };
+    ///     request.reply_to.try_send(response).unwrap();
+    ///
+    ///     (cell, ())
+    /// }
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     let ActorToSpawn { task, .. } = actum(root_actor);
+    ///     task.run_task().await;
+    /// }
+    /// ```
     fn create_child<M, F, Fut, Ret>(
         &mut self,
         f: F,
