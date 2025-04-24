90b906842cd4feaee3e0bb1e20d4a0219189f698
andreastedile <andrea.stedile@unitn.it>
2025-04-24 22:39:49 +0200

    Add lib doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+/// Implementation of core traits for actors.
 pub mod actor;
+/// Implementation of core traits for actors with instrumentation for testing their behavior.
 pub mod actor_test;
+/// Definition of core traits and structs.
 pub mod core;
 pub mod prelude;
