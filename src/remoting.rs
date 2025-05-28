65ca646cf04cf3c9503c8809d6509d5bf9783361
andreastedile <andrea.stedile@unitn.it>
2025-05-28 16:53:53 +0200

    Do not fix K and MappedActorRef to make the interface more flexible
    Let's recap the first solution I architected.
    
    The idea is to allow an actor with a generic interface (that is, ActorRef<M> type) to register to the remoting service by passing a key identifier K (bounded by Hash) and the ActorRef itself.
    The user-defined async closure of the remoting service will receive bytes from the network, parse them and compose a Letter<K, MessageTypes> with recipient K and message MessageTypes. Then, the remoting service will get the ActorRef identified by K from a HashMap<K, ActorRef<?>> and dispatch the message to it. But what should the type of this ActorRef be? This is complicated due to the fact that any actor (hence, any ActorRef<M1>, ActorRef<M2>, and so on) should be allowed to register to the service.
    
    Since we cannot know all such types in advance, we need to resort to a "general enough" type.
    First, I devised a MappedActorRef<MessageTypes> and made the remoting service accept MappedActorRef<MessageTypes>.
    An actor which is willing to register should first convert ("widen") its ActorRef<M> to a general, wider MappedActorRef<MessageTypes> and establish a conversion from MessageTypes to M. The remoting service will therefore send the message through this general interface and not incur in the aforementioned issue about types, which is going to be taken care of by the conversion code.
    This is achieved with a trait bound and dynamic dispatch:
    
    ```rust
    pub fn widen<Supertype>(mut self) -> MappedActorRef<Supertype>
    where
    	M: TryFrom<Supertype> + Send + 'static,
    	Supertype: From<M>,
    {
    	MappedActorRef {
    	    f: Arc::new(Mutex::new(move |supertype: Supertype| {
    			let m = supertype.try_into().unwrap_or_else(|_| {
    				panic!(
    				"conversion failed from {} to {}",
    				std::any::type_name::<Supertype>(),
    				std::any::type_name::<M>()
    				)
    			});
    			self.try_send(m).map_err(|m| m.into())
    	    })),
    	}
    }
    ```
    
    There are two downsides:
    1. The major downside is that the conversion code in the closure will mostly consist of unwrapping the correct Supertype variant to an M which is useful for the actor identified by K.
    2. The implementation of MappedActorRef uses dynamic dispatch.
    
    Can we architect another solution which does not incur in neither downside?
    
    The problem originates from the fact that we used a HashMap<K, ActorRef<MessageTypes>> in the remoting service and accepted ActorRef<MessageTypes>.
    What if:
    1. The type of the registrant was not fixed and did not require K (that is, was a generic RegistrantType instead), and
    2. The type of data structure was not fixed as a HashMap (that is, was a generic RegistrarType instead), and
    3. The user could define its own function (RegistrarType, LetterType) for dispatching a letter (LetterType) to a registered actor (a RegistrantType in a RegistrarType)?
    
    RegistrarType should be bounded by a Registrar trait with two methods: register(RegistrantType) and unregister(RegistrantType) (which, in case of a HashMap, will correspond to to insert and remove operations).
    
    We could, for example, group all different ActorRef types in an enum:
    
    ```rust
    enum MyRegistrantType {
    	String(ActorRef<String>),
    	U32(ActorRef<u32>),
    }
    ```
    
    First, this would solve the dynamic dispatch problem.
    
    Second, by doing so, we are also removing a lot of rigidity from the architecture:
    - Not all subscribers need be identified by a key identifier!
    - Sometimes, all subscribers are known in advance, and could (or should) be preloaded and passed in the RegistrarType itself, so that incoming network messages are not lost.
    - There could even be just one subscriber, and the HashMap solution would be inefficient.

--- src/remoting.rs
+++ src/remoting.rs
@@ -1,34 +1,32 @@
-use crate::core::actor_ref::MappedActorRef;
-use crate::prelude::{ReceiveMessage, Recv};
+use crate::core::receive_message::ReceiveMessage;
+use crate::core::receive_message::Recv;
 use futures::future::Either;
 use futures::pin_mut;
-use std::collections::HashMap;
 
-pub enum Command<K, MessageTypes> {
-    Register {
-        who: K,
-        actor_ref: MappedActorRef<MessageTypes>,
-    },
-    Unregister {
-        id: K,
-    },
+pub enum Command<RegistrantType> {
+    Register { who: RegistrantType },
+    Unregister { who: RegistrantType },
 }
 
-struct Letter<K, MessageTypes> {
-    id: K,
-    message: MessageTypes,
+/// For registering actors which are interested in receiving messages from remote sources and
+/// unregistering those which are no more interested.
+pub trait Registrar<RegistrantType> {
+    fn register(&mut self, who: RegistrantType);
+    fn unregister(&mut self, who: RegistrantType);
 }
 
-async fn actum_remoting<K, MessageTypes, R>(
+async fn actum_remoting<RegistrantType, LetterType, RegistrarType, DispatchLetterType, R>(
     mut receiver: R,
-    mut receive_letter: impl AsyncFnMut() -> Letter<K, MessageTypes>,
+    mut registrar: RegistrarType,
+    mut receive_letter: impl AsyncFnMut() -> LetterType,
+    mut dispatch_letter: DispatchLetterType,
 ) where
-    K: Eq + std::hash::Hash + Send + 'static,
-    MessageTypes: Send + 'static,
-    R: ReceiveMessage<Command<K, MessageTypes>>,
+    RegistrantType: Send + 'static,
+    LetterType: Send + 'static,
+    RegistrarType: Registrar<RegistrantType>,
+    DispatchLetterType: FnMut(&mut RegistrarType, LetterType),
+    R: ReceiveMessage<Command<RegistrantType>>,
 {
-    let mut actor_map = HashMap::<K, MappedActorRef<MessageTypes>>::new();
-
     'outer: loop {
         let letter_future = receive_letter();
         pin_mut!(letter_future);
@@ -39,14 +37,11 @@ async fn actum_remoting<K, MessageTypes, R>(
             match select {
                 Either::Left((Recv::Message(command), ..)) => {
                     match command {
-                        Command::Register {
-                            who: id,
-                            actor_ref: who,
-                        } => {
-                            actor_map.insert(id, who);
+                        Command::Register { who } => {
+                            registrar.register(who);
                         }
-                        Command::Unregister { id } => {
-                            actor_map.remove(&id);
+                        Command::Unregister { who } => {
+                            registrar.unregister(who);
                         }
                     }
                     //
@@ -55,11 +50,7 @@ async fn actum_remoting<K, MessageTypes, R>(
                     break 'outer;
                 }
                 Either::Right((letter, ..)) => {
-                    if let Some(actor_ref) = actor_map.get_mut(&letter.id) {
-                        let _ = actor_ref.try_send(letter.message);
-                    } else {
-                        // Todo: is the message lost? Should we resend it?
-                    }
+                    dispatch_letter(&mut registrar, letter);
 
                     break 'inner;
                 }
@@ -68,55 +59,222 @@ async fn actum_remoting<K, MessageTypes, R>(
     }
 }
 
+pub mod kv {
+    use super::*;
+    use crate::prelude::MappedActorRef;
+    use std::collections::HashMap;
+    use std::fmt::Debug;
+    use std::hash::Hash;
+
+    pub struct Letter<ActorIdentifier, MessageTypes> {
+        pub(crate) recipient: ActorIdentifier,
+        pub(crate) message: MessageTypes,
+    }
+
+    pub struct IdentifiedMappedActorRef<ActorIdentifier, MessageTypes> {
+        pub id: ActorIdentifier,
+        pub mapped_actor_ref: MappedActorRef<MessageTypes>,
+    }
+
+    impl<ActorIdentifier, MessageTypes> Registrar<IdentifiedMappedActorRef<ActorIdentifier, MessageTypes>>
+        for HashMap<ActorIdentifier, MappedActorRef<MessageTypes>>
+    where
+        ActorIdentifier: Eq + Hash,
+    {
+        fn register(&mut self, who: IdentifiedMappedActorRef<ActorIdentifier, MessageTypes>) {
+            self.insert(who.id, who.mapped_actor_ref);
+        }
+        fn unregister(&mut self, who: IdentifiedMappedActorRef<ActorIdentifier, MessageTypes>) {
+            self.remove(&who.id);
+        }
+    }
+
+    pub async fn actum_remoting_kv<ActorIdentifier, MessageTypes, R>(
+        receiver: R,
+        registrar: HashMap<ActorIdentifier, MappedActorRef<MessageTypes>>,
+        receive_letter: impl AsyncFnMut() -> Letter<ActorIdentifier, MessageTypes>,
+    ) where
+        ActorIdentifier: Debug + Eq + Hash + Send + 'static,
+        MessageTypes: Send + 'static,
+        R: ReceiveMessage<Command<IdentifiedMappedActorRef<ActorIdentifier, MessageTypes>>>,
+    {
+        actum_remoting(receiver, registrar, receive_letter, |registrar, letter| {
+            let Some(mapped_actor_ref) = registrar.get_mut(&letter.recipient) else {
+                tracing::warn!("cannot dispatch message to recipient = {:?}", letter.recipient);
+                return;
+            };
+            let _ = mapped_actor_ref.try_send(letter.message);
+        })
+        .await;
+    }
+}
+
 #[cfg(test)]
-mod tests {
+mod remoting_tests {
     use super::*;
     use crate::prelude::*;
+    use tokio::net::UdpSocket;
+
+    type MyLetterType = String;
+
+    type MyRegistrantType = ActorRef<String>;
+
+    struct MyRegistrarType {
+        registered: Option<ActorRef<String>>,
+    }
+
+    impl Registrar<ActorRef<String>> for MyRegistrarType {
+        fn register(&mut self, _: ActorRef<String>) {
+            unreachable!()
+        }
+
+        fn unregister(&mut self, _: ActorRef<String>) {
+            unreachable!()
+        }
+    }
+
+    fn my_dispatch_letter(registrar: &mut MyRegistrarType, letter: String) {
+        if let Some(registered) = &mut registrar.registered {
+            let _ = registered.try_send(letter);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_it() {
+        let sock = UdpSocket::bind("0.0.0.0:8080").await.unwrap();
+        let mut buf = [0; 1024];
+        let my_receive_letter = async move || {
+            let (len, ..) = sock.recv_from(&mut buf).await.unwrap();
+            String::from_utf8_lossy(&buf[..len]).to_string()
+        };
+
+        // echo -n "hello, world!" >/dev/udp/0.0.0.0/8080
+        // echo -n "what's up?" >/dev/udp/0.0.0.0/8080
+        // echo -n "stop" >/dev/udp/0.0.0.0/8080
+
+        let CreateActorResult { task, .. } = actum::<(), _, _, ()>(|mut cell, receiver, me| async move {
+            drop(receiver);
+            drop(me);
+
+            // instantiate the actor which uses the remoting service
+            let CreateActorResult {
+                task,
+                actor_ref: registrant,
+            } = cell
+                .create_child::<String, _, _, ()>(|cell, mut receiver, me| async move {
+                    drop(me);
+                    while let Recv::Message(m) = receiver.recv().await {
+                        println!("child: received {}", m);
+                        if m == "stop" {
+                            println!("stopping");
+                            break;
+                        }
+                    }
+
+                    (cell, ())
+                })
+                .await;
+            let registrant_handle = tokio::spawn(task.run_task());
+
+            // instantiate the actum remoting service
+            let CreateActorResult {
+                task,
+                actor_ref: receptionist_ref,
+            } = cell
+                .create_child::<Command<MyRegistrantType>, _, _, ()>(|cell, receiver, me| async move {
+                    drop(me);
+
+                    // preload the registrar
+                    let registrar = MyRegistrarType {
+                        registered: Some(registrant),
+                    };
+
+                    actum_remoting::<MyRegistrantType, MyLetterType, MyRegistrarType, _, _>(
+                        receiver,
+                        registrar,
+                        my_receive_letter,
+                        my_dispatch_letter,
+                    )
+                    .await;
+
+                    (cell, ())
+                })
+                .await;
+            let receptionist_handle = tokio::spawn(task.run_task());
+
+            // to prevent the remoting service actor from returning due to "no more senders",
+            // we must keep a reference in scope up until the users of the service are done with it.
+            registrant_handle.await.unwrap();
+            drop(receptionist_ref);
+
+            (cell, ())
+        });
+
+        task.run_task().await;
+    }
+}
+
+#[cfg(test)]
+mod remoting_kv_tests {
+    use super::*;
+    use crate::prelude::*;
+    use crate::remoting::kv::{IdentifiedMappedActorRef, Letter, actum_remoting_kv};
     use either::Either;
+    use std::collections::HashMap;
     use tokio::net::UdpSocket;
 
     #[tokio::test]
     async fn test_it() {
         let sock = UdpSocket::bind("0.0.0.0:8080").await.unwrap();
         let mut buf = [0; 1024];
-        let f = async move || {
+        let my_receive_letter = async move || {
             let (len, ..) = sock.recv_from(&mut buf).await.unwrap();
             Letter::<u32, Either<String, u32>> {
-                id: 0,
+                recipient: 0,
                 message: Either::Left(String::from_utf8_lossy(&buf[..len]).to_string()),
             }
         };
 
         // echo -n "hello, world!" >/dev/udp/0.0.0.0/8080
         // echo -n "what's up?" >/dev/udp/0.0.0.0/8080
+        // echo -n "stop" >/dev/udp/0.0.0.0/8080
 
         let CreateActorResult { task, .. } = actum::<(), _, _, ()>(|mut cell, receiver, me| async move {
             drop(receiver);
             drop(me);
 
+            // instantiate the actum remoting service
             let CreateActorResult {
                 task,
                 actor_ref: mut receptionist_ref,
             } = cell
-                .create_child::<Command<u32, Either<String, u32>>, _, _, ()>(|cell, receiver, me| async move {
+                .create_child::<_, _, _, ()>(|cell, receiver, me| async move {
                     drop(me);
 
-                    actum_remoting(receiver, f).await;
+                    let registrar = HashMap::new();
+
+                    actum_remoting_kv::<u32, Either<String, u32>, _>(receiver, registrar, my_receive_letter).await;
 
                     (cell, ())
                 })
                 .await;
             tokio::spawn(task.run_task());
 
+            // instantiate the actor which uses the remoting service
             let CreateActorResult { task, .. } = cell
-                .create_child::<String, _, _, ()>(|cell, mut receiver, me| async move {
+                .create_child::<_, _, _, ()>(|cell, mut receiver, me| async move {
                     let _ = receptionist_ref.try_send(Command::Register {
-                        who: 0,
-                        actor_ref: me.widen(Either::unwrap_left),
+                        who: IdentifiedMappedActorRef {
+                            id: 0,
+                            mapped_actor_ref: me.widen(Either::unwrap_left),
+                        },
                     });
 
                     while let Recv::Message(m) = receiver.recv().await {
                         println!("child: received {}", m);
+                        if m == "stop" {
+                            break;
+                        }
                     }
 
                     (cell, ())
